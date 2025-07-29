ba724f307c377a77ec04ceae7aa9321aeecf77d3
iceXshadow <devxicy@gmail.com>
2025-07-30 00:12:57 +0530

    feat: validation

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+	...compat.config({
+		extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+		rules: {
+			semi: ["error"],
+			quotes: ["error", "double"],
+			"prefer-arrow-callback": ["error"],
+			"prefer-template-literals": ["error"],
+		},
+	}),
+];
+
+export default eslintConfig;
